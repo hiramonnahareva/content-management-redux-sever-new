d0ecfcfbc1d4b6c82639bcdc92784e4afc6697e0
Hiramon Nahar Eva <hiramonnahareva@gmail.com>
2023-01-14 13:32:20 +0600

    add env

--- index.js
+++ index.js
@@ -10,11 +10,10 @@ require('dotenv').config();
 
 app.use(cors())
 app.use(express.json());
-
-// // mongodb connection
+// mongodb connection
 
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u3hia.mongodb.net/?retryWrites=true&w=majority`;
-const uri = `mongodb+srv://dbuse1:trD8rPchraKwIgen@cluster0.u3hia.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u3hia.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
